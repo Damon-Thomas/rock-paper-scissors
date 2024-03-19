4e0ab0b4f947513cb2d4f8e02363df31f6fa6a0d
Damon Thomas <damon22t@gmail.com>
2024-03-19 16:57:46 -0400

    add game result function in js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-// Get computer's random choice between 0-2
+// Get computer's random choice 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice < 1) {
@@ -11,11 +11,30 @@ function getComputerChoice() {
     else {
         return "internal error";}
 }
-console.log(getComputerChoice());
+
+
 //request user choice and prep for comparison
 function playerSelection() {
     let player_choice = prompt("Rock, Paper, or Scissors");
     return player_choice.toLowerCase();
 }
 
-console.log(playerSelection());
+
+
+
+
+ // result of game loss 0, win 1, tie 2
+function playerWins(userInput, computerInput) {
+    if (userInput === computerInput) {
+        return 2}
+    else if 
+      ((userInput === "rock" && computerInput === "scissors") || 
+      (userInput === "paper" && computerInput === "rock") || 
+      (userInput === "scissors" && computerInput === "paper")) {
+        return (1);
+    }
+    else {
+        return 0
+    }   
+}
+console.log(playerWins(getComputerChoice(), playerSelection()))
